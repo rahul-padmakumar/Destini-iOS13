32114d2902463c48ce45e8ae3997f3486e3981fe
rahulp <rahulp@qburst.com>
2025-03-14 16:04:32 +0530

    MVC implementation completed

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain: StoryBrain = StoryBrain()
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let choice = sender.currentTitle!
+        storyBrain.nextStory(choice: choice)
+        updateUI()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
-
+    func updateUI(){
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
 }
 
